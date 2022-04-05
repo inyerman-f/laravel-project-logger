3023c83fb75a1e23f10b23d2fe52d67e25a2de7a
Inyerman Fonseca <inyermanfm@gmail.com>
2022-04-05 16:45:26 -0400

    Cleaned up the edit project interface.

--- resources/js/components/EditProject.vue
+++ resources/js/components/EditProject.vue
@@ -1,29 +1,33 @@
 <template>
     <div class="container">
-        <h1>Edit Project, with id: {{$route.params.id }}</h1>
-        {{projectDetails}}
-        <div>
-            <label for="project_name">Project Name</label>
-            <input name="project_name" type="text" v-model="projecto.project_name"  >
-        </div>
-        <div>
-            <label for="project_description">Project Description</label>
-            <input type="text" v-model="projecto.project_description">
-        </div>
-        <button @click="updateProject($route.params.id )">Update Project</button>
+        <h1>Edit Project {{projectName}}, with id: {{$route.params.id }}</h1>
+        <form>
+            <div>
+                <label for="project_name_edit" >Project Name</label>
+                <input name="project_name" type="text" v-model="projecto.project_name" id="project_name_edit" ref="projectNameInputRef">
+            </div>
+            <div>
+                <label for="project_description_edit" >Project Description</label>
+                <input type="text" v-model="projecto.project_description" id="project_description_edit" ref="projectDescriptionInputRef">
+            </div>
+            <div>
+                <button @click="updateProject($route.params.id)" class="btn btn-primary" >Update Project</button>
+            </div>
+        </form>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            projectDetails : [],
+            projectDescription : [],
+            projectName:[],
             projecto: {
                 project_name: '',
                 project_description: '',
             },
             input: {
-                name: "", // or pre-fill with other default value like `lorem`
+                project_name: "derp", // or pre-fill with other default value like `lorem`
 
             },
         }
@@ -45,7 +49,8 @@ export default {
         getProjectDetails(project_id){
             axios.get('/api/projects/'+project_id,)
                 .then((res) => {
-                    this.projectDetails = res.data['data'];
+                    this.projectName = res.data['project_name'];
+                    this.projectDescription = res.data['project_description'];
                     this.loading = false;
                 })
                 .catch((err) => console.error(err));
